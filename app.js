6259d732c8e4afc04e1f936efaae3caa0068cf0b
Daymi <dsablonm@gmail.com>
2023-02-06 00:06:11 -0500

    DEL: Removing unnecessary await

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.set('strictQuery', true);
 
 const start = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
